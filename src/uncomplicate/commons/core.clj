ff6c22c97802bdff028409d8bd8c7f15f590d15b
Dragan Djuric <dragandj@gmail.com>
2020-08-25 15:22:59 +0200

    Move Viewable/view here from neanderthal.

--- src/uncomplicate/commons/core.clj
+++ src/uncomplicate/commons/core.clj
@@ -166,6 +166,22 @@
   (mmap [this] [this flags])
   (unmap [this mapped]))
 
+;; =================== Viewable ========================================
+
+(defprotocol Viewable
+  "Attach a default dense structure to the raw data of `x`. `x` can be anything that implements
+  Viewable, such as DirectByteBuffer.
+
+  Changes to the resulting object affect the source `x`, even the parts of data that might not
+  be accessible by `x`. Use with caution!
+
+  view always creates a new instance that reuses the master's data,
+  but releasing a view never releases the master data.
+
+      (view (buffer (vctr float-factory 1 2 3)))
+  "
+  (view [this]))
+
 ;; =================== Array wrappers ==================================
 
 (defn wrap-byte ^bytes [^long x]
