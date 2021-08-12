3cb2c76f44158366820145272c681d9007bdd928
Dragan Djuric <dragandj@gmail.com>
2021-08-12 02:22:31 +0200

    Release 0.12.1.

--- project.clj
+++ project.clj
@@ -6,26 +6,26 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/commons "0.12.0-SNAPSHOT"
+(defproject uncomplicate/commons "0.12.1"
   :description "Common Uncomplicate utilities."
   :url "https://github.com/uncomplicate/commons"
   :scm {:name "git"
         :url "https://github.com/uncomplicate/commons"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
 
   :codox {:src-dir-uri "http://github.com/uncomplicate/commons/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-path "docs/codox"}
 
   :profiles {:dev {:plugins [[lein-midje "3.2.1"]
-                             [lein-codox "0.10.6"]]
+                             [lein-codox "0.10.7"]]
                    :global-vars {*warn-on-reflection* true
                                  *assert* true
                                  *unchecked-math* :warn-on-boxed
                                  *print-length* 128}
-                   :dependencies [[midje "1.9.9"]]
+                   :dependencies [[midje "1.10.4"]]
                    :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
                                         #_"--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"]}}
 
